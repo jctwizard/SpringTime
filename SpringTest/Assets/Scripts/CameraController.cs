db98e32dbc935f73a31fe35bce8cfd3d66b2c85a
Chris McMath <chrismcmath@gmail.com>
2018-09-29 16:28:10 +0100

    Add spring test project

---
+++ SpringTest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Transform Follow;
+
+	void Update()
+	{
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Lerp(pos.x, Follow.position.x, 0.1f);
+		transform.position = pos;
+	}
+}
---
+++ SpringTest/Assets/Scripts/RigidbodyTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		//GetComponent<Rigidbody2D>().isKinematic = true;
+		GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			Debug.LogFormat("appling force");
+			GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10f, ForceMode2D.Impulse);
+		}
+		
+	}
+}
